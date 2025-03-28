6638f9424fc84d5d40451b0873877142365c8a50
everlyn <everlynsimiyu15@gmail.com>
2025-03-28 14:36:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+const products = [
+  { name: "Shampoo", price: 4.99 },
+  { name: "Donuts", price: 7.99 },
+  { name: "Cookies", price: 6.49 },
+  { name: "Bath Gel", price: 13.99 },
+];
+
+const totalAmount = products.reduce((total, product) => total + product.price, 0);
+
+const couponLocations = [
+  { room: "Living room", amount: 5 },
+  { room: "Kitchen", amount: 2 },
+  { room: "Bathroom", amount: 1 },
+  { room: "Master bedroom", amount: 7 },
+];
+
+const totalCoupons = couponLocations.reduce((total, location) => total + location.amount, 0);
+
+console.log("Total Batteries:", totalBatteries);
+console.log("Total Price of Products:", totalAmount);
+console.log("Total Coupons:", totalCoupons);
